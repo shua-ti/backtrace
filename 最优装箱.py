0fad01b44263dc4365fb4fa4cdcbd59faf6d3f65
wanpeng16 <wanpengwx@163.com>
2016-12-19 17:31:09 +0800

    边界条件

--- 最优装箱.py
+++ 最优装箱.py
@@ -9,23 +9,24 @@ def loading(weight,c1):
     pattern = []
     p = [0] * len(weight)
     def load(c1, k=0, cw=0):
-        if cw > c1: return
+        if cw > c1: return    #边界体条件
         if k == len(weight):
             if cw <= c1:
                 bestw.append(cw)
                 p_copy = list(p)
                 pattern.append(p_copy)
             return
-
+        #第k个货物装上箱
         cw += weight[k]
         p[k] = 1
         load(c1, k + 1, cw)
-        cw -= weight[k]
+        #第k个货物不装上箱
+        cw -= weight[k]              
         p[k] = 0
         load(c1, k + 1, cw)
     load(c1)
     if sum(weight)-max(bestw) <=c2:
-        print "可以顺利装箱："
+        print "可以顺利装箱：\n",
     return pattern[bestw.index(max(bestw))]
 
 if __name__=="__main__":
